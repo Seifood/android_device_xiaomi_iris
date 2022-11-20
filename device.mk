64b5d2e790f74e7c49d2228ea960a719f89f8f61
Droneship <81514881+Seifood@users.noreply.github.com>
2022-11-20 15:30:07 +0200

    Iris: update fingerprint

--- device.mk
+++ device.mk
@@ -195,8 +195,11 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint@2.1-service \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FM
 PRODUCT_PACKAGES += \
