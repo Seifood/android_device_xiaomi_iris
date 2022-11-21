314d47a6c7210341ea313ec9476358fd5136e144
Droneship <81514881+Seifood@users.noreply.github.com>
2022-11-22 01:07:52 +0200

    iris: build missing soundtrigger
    [  641.029356] init: Control message: Could not find 'android.hardware.soundtrigger@2.0::ISoundTriggerHw/default' for ctl.interface_start from pid: 631 (/system/bin/hwservicemanager)

--- device.mk
+++ device.mk
@@ -37,6 +37,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@4.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
