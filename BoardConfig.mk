342ddbb39b96f4efab251b266826be6b1857457d
Droneship <81514881+Seifood@users.noreply.github.com>
2022-11-22 15:39:02 +0200

    iris: revert: remove fcm

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,8 +87,8 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-#DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    #$(DEVICE_PATH)/framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 ODM_MANIFEST_SKUS += nfc
 ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/manifest_nfc.xml
