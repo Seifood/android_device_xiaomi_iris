52a13e950913de724297e9687c70ad8c3b27e670
Droneship <81514881+Seifood@users.noreply.github.com>
2022-12-27 08:20:37 +0200

    iris: build vendor.qti.hardware.vibrator.service

--- device.mk
+++ device.mk
@@ -499,7 +499,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_holi
+    vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
